4e293ca459d84e06f430ff06d633f870851399b0
Abhishek <abhishek.nayak560@gmail.com>
2022-05-26 22:12:36 +0530

    Ui Profile Image Placed

---
+++ app/src/main/java/com/example/uidesign/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.example.uidesign;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Button login=findViewById(R.id.login);
+        TextView gotosignup=findViewById(R.id.gotoSignup);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),ProfileActivity.class));
+            }
+        });
+        gotosignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+}
